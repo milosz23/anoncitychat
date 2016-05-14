197a133e466e145dee554eb6cf566cd09ed44f94
milosz <milosz@meta.ua>
2016-05-14 16:55:25 +0300

    test gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles:build', function() {
+     return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/'))
+});
+
+gulp.task('styles:watch',function() {
+    gulp.watch('sass/**/*.scss',['styles:build']);
+});
