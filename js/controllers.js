7c387669d0cc4200f3926aad5666683c8367e0c0
milosz <milosz@meta.ua>
2016-06-10 22:11:21 +0300

    Some messy logic before websockets

--- js/controllers.js
+++ js/controllers.js
@@ -15,7 +15,8 @@ angular.module('cityChat')
     };
 
     this.setLocation = function(){
-        $http.get('https://fun-milosz.c9.io/ip')
+        var apiUrl = 'http://test.vi:3000';//'https://fun-milosz.c9.io'
+        $http.get(apiUrl + '/ip')
             .then(function(ip) {
                 var ipLocationApiQuery = 'http://ip-api.com/json/' + ip.data + '?fields=258047';
                 $http.get(ipLocationApiQuery)
@@ -31,8 +32,16 @@ angular.module('cityChat')
 })
 
 //------------------------------------------------------------------------
-.controller('FindController', function() {
+.controller('FindController', function($scope, $http, $state) {
     console.log('FindController loaded');
+
+    $scope.findChat = function() {
+        var apiUrl = 'http://test.vi:3000';//'https://fun-milosz.c9.io'
+        $http.post(apiUrl + '/find', {name: localStorage.user, city: localStorage.city})
+            .then(function(responseData) {
+                console.log(responseData);
+            });
+    };
 })
 .controller('ChatController', function() {
     console.log('ChatController loaded');
--- nodeApp.js
+++ nodeApp.js
@@ -1,14 +1,65 @@
 var fs = require('fs'),
     http = require('http'),
-//cloud9 deploy or local 3000 port
+    //cloud9 deploy or local 3000 port
     port = process.env.PORT ? process.env.PORT : 3000;
 
+    var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 /*
  Simple server *********************************************************************
  */
 console.log('Starting server on port: ' + port);
 http.createServer(function (req, res) {
-    if(req.method==='POST') {
+    if(req.method==='POST' && req.url === '/find') {
+        req.on('data', function (data) {
+            console.log("POST data: " + data);
+
+
+
+            var dataDecoded = JSON.parse(data);
+            // Connection URL
+            var url = 'mongodb://localhost:27017/test';
+            // Use connect method to connect to the Server
+            MongoClient.connect(url, function (err, db) {
+                console.log("Connected correctly to server");
+                var collection = db.collection("ololo");
+                //1.Try to find
+                collection.find({
+                    city: dataDecoded.city,
+                    user2: null
+                }).toArray(function (err, docs) {
+                    if(docs[0] === undefined) {
+                        collection.insertOne({
+                                user1: dataDecoded.name,
+                                user2: null,
+                                city:  dataDecoded.city
+                            }, function(err,result){
+                                db.close();
+                                res.writeHead(200);
+                                res.end('waiting');
+                            });
+                    } else {
+                        collection.updateOne(
+                            {
+                                city: dataDecoded.city,
+                                user2: null
+                            },
+                            {
+                                $set: { "user2": dataDecoded.name }
+                            }, function(err, results) {
+                                //console.log(results);
+                                db.close();
+                                res.writeHead(200);
+                                res.end('connected');
+                            });
+                    }
+                });
+
+
+            });
+
+        })
+    } else if(req.method==='POST') {
         req.on('data', function (data) {
             console.log("POST data: " + data);
             var date = new Date();
@@ -20,7 +71,7 @@ http.createServer(function (req, res) {
             res.writeHead(200);
             res.end(data);
         });
-    } if (req.url === '/ip') {
+    } else if (req.url === '/ip') {
         res.setHeader('Access-Control-Allow-Origin', '*');
         var ip = req.headers['x-forwarded-for'] ||
             req.connection.remoteAddress ||
